929c02436cefd3c58f939f6160cfb4e080c71697
vgoetz <viktorgoetz@t-online.de>
2016-07-24 13:26:46 +0200

    add new Entity to EntityManager

--- ComponentBasedSystem.Core/EntityManager.cs
+++ ComponentBasedSystem.Core/EntityManager.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ComponentBasedSystem.Core {
 
     public interface IEntityManager {
-        
+        long AddEntity(IEnumerable<IEntityComponent> entityComponentList);
+        int GetEntityCount();
+        IList<IEntityComponent> GetEntityComponents(int id);
     }
 
     public class EntityManager : IEntityManager {
-        private Dictionary<int, List<IEntityComponent>> _entities;
+        private readonly Dictionary<long, List<IEntityComponent>> _entities;
 
         public EntityManager() {
-            _entities = new Dictionary<int, List<IEntityComponent>>();
+            _entities = new Dictionary<long, List<IEntityComponent>>();
+        }
+
+        public long AddEntity(IEnumerable<IEntityComponent> entityComponentList) {
+
+            long newKey = 0;
+
+            if (_entities.Any()) {
+                newKey = _entities.LastOrDefault().Key + 1;
+            }
+
+            _entities.Add(newKey, entityComponentList.ToList());
+
+            return newKey;
+        }
+
+        public int GetEntityCount() {
+            return _entities.Count;
+        }
+
+        public IList<IEntityComponent> GetEntityComponents(int id) {
+
+            if (_entities.ContainsKey(id)) {
+                return _entities[id];
+            }
+
+            return new List<IEntityComponent>();
         }
     }
 }
---
+++ ComponentBasedSystem.UnitTest/EntityManagerFixture.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using Autofac;
+using ComponentBasedSystem.Core;
+using NUnit.Framework;
+
+namespace ComponentBasedSystem.UnitTest {
+
+    [TestFixture]
+    public class EntityManagerFixture {
+
+
+        [Test]
+        public void AddNewEntityToEntityManager_DefaultListOfEntitiesIsEmpty() {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EntityManager>().As<IEntityManager>().SingleInstance();
+            var container = builder.Build();
+
+            IEntityManager entityManager = container.Resolve<IEntityManager>();
+
+            Assert.That(entityManager.GetEntityCount(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AddNewEntityToEntityManager_DefaultComponentListOfNotFoundEntityIsEmpty() {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EntityManager>().As<IEntityManager>().SingleInstance();
+            var container = builder.Build();
+
+            IEntityManager entityManager = container.Resolve<IEntityManager>();
+
+            Assert.That(entityManager.GetEntityCount(), Is.EqualTo(0));
+
+            var componentsOfEntity = entityManager.GetEntityComponents(0);
+            Assert.That(componentsOfEntity, Is.Not.Null);
+            Assert.That(componentsOfEntity, Is.Empty);
+        }
+
+        [Test]
+        public void AddNewEntityToEntityManager_CountWillBe1() {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EntityManager>().As<IEntityManager>().SingleInstance();
+            var container = builder.Build();
+
+            IEntityManager entityManager = container.Resolve<IEntityManager>();
+
+            entityManager.AddEntity(new List<IEntityComponent> { new HealthComponent(10, 5) });
+
+            Assert.That(entityManager.GetEntityCount(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddNewEntityToEntityManager_IncrementedId() {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EntityManager>().As<IEntityManager>().SingleInstance();
+            var container = builder.Build();
+
+            IEntityManager entityManager = container.Resolve<IEntityManager>();
+
+            var id = entityManager.AddEntity(new List<IEntityComponent> { new HealthComponent(10, 5) });
+
+            Assert.That(id, Is.EqualTo(0));
+
+            id = entityManager.AddEntity(new List<IEntityComponent> { new HealthComponent(10, 5) });
+
+            Assert.That(id, Is.EqualTo(1));
+        }
+
+
+        [Test]
+        public void AddNewEntityToEntityManager_GetAddedComponentsOfNewEntity() {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EntityManager>().As<IEntityManager>().SingleInstance();
+            var container = builder.Build();
+
+            IEntityManager entityManager = container.Resolve<IEntityManager>();
+
+            entityManager.AddEntity(new List<IEntityComponent> { new HealthComponent(10, 5) });
+
+            var componentsOfEntity = entityManager.GetEntityComponents(0);
+            
+            Assert.That(componentsOfEntity.Count, Is.EqualTo(1));
+            Assert.That(componentsOfEntity[0], Is.TypeOf(typeof(HealthComponent)));
+        }
+    }
+}
