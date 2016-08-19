03fd73150dd82532458a11d705e8cc3b048835c0
vgoetz <viktorgoetz@t-online.de>
2016-08-19 14:27:09 +0200

    Test für Bootstrapper

---
+++ ComponentBasedSystem.UnitTest/BootstrapperFixture.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using ComponentBasedSystem.BootstrapperAndMondules;
+using ComponentBasedSystem.Core;
+using NUnit.Framework;
+
+namespace ComponentBasedSystem.UnitTest {
+
+    [TestFixture]
+    public class BootstrapperFixture {
+
+        [Test]
+        public void ResolveEntityManagerWithAutofacAfterBootstrapperTest() {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.CreateContainer();
+            var entityManager = container.Resolve<IEntityManager>();
+
+            Assert.That(entityManager, Is.Not.Null);
+            Assert.That(entityManager, Is.TypeOf(typeof(EntityManager)));
+        }
+    }
+}
